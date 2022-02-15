902903c8212b786c8abeeb29eed38e9ff7f4f692
Luiz Calaça <luizcalaca@Luizs-MacBook-Air.local>
2022-02-14 22:55:44 -0300

    docs: Adapting for Swagger and OpenAPI

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const {otherError}= require('./middlewares/error')
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", personController)
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.use(otherError);
 
 app.listen(PORT, () => {
